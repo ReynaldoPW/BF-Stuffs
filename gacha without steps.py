296f5909911b826f14955b0d1d71168cfa877bb2
Wirayuda24 <45593381+Wirayuda24@users.noreply.github.com>
2019-06-28 08:47:58 +0700

    Update gacha without steps.py

--- gacha without steps.py
+++ gacha without steps.py
@@ -12,7 +12,7 @@ foundFilesCount = 0
 def gatherUnitIlls(filename):
 
 	try:
-		f = urllib.request.urlopen("http://dlc.bfglobal.gumi.sg/content/gacha/" + filename)
+		f = urllib.request.urlopen("http://dv5bk1m8igv7v.cloudfront.net/asset/2200/content/gacha/" + filename)
 		#f = urllib.urlopen("http://v2.cdn.android.brave.a-lim.jp//unit/img/" + filename)
 		fetched = f.read()
 		f.close()
@@ -186,7 +186,7 @@ if __name__ == "__main__":
 		threadList.append(Thread(target = gatherUnitIlls, args = (filename,)))        
 
 	for threadIndividual in threadList:
-		sleep(0.1)
+		sleep(0.03)
 		threadIndividual.start()
 	
 	for threadIndividual in threadList:
