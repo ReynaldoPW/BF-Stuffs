9de48b98260a60fad34b2b55570ffc3f139a2822
Reynaldo Paulini Wonges <reynaldopaulini@yahoo.com>
2019-11-20 14:02:27 +0700

    20 nov maint part 1 + update cdn link

--- sphere.py
+++ sphere.py
@@ -12,7 +12,7 @@ foundFilesCount = 0
 def gatherSphereIlls(filename):
 
     try:
-        f = urllib.request.urlopen("http://dv5bk1m8igv7v.cloudfront.net/asset/2600/content/item/" + filename)
+        f = urllib.request.urlopen("http://dv5bk1m8igv7v.cloudfront.net/asset/2700/content/item/" + filename)
         fetched = f.read()
         f.close()
         
