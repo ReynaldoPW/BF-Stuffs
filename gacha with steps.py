0717b95116f47d28301034b94aed7fca35db19f7
Reynaldo Paulini Wonges <reynaldopaulini@yahoo.com>
2020-01-28 12:05:03 +0700

    Update gacha with steps.py

--- gacha with steps.py
+++ gacha with steps.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
 	print("Already exist: ", len(alreadyExistFiles))
 
 	
-	for i in range(22, 32):
+	for i in range(28, 32):
 		for j in range(1,36):
 		   
 			s = "gacha_img_202001%02dA_s%d.png" % (i, j)
