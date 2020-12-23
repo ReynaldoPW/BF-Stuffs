4a259e5d71e3c786e2b645cc7b2d07c33937bbd5
Reynaldo Paulini Wonges <reynaldopaulini@yahoo.com>
2020-12-23 12:48:07 +0700

    update assets

--- sphere.py
+++ sphere.py
@@ -12,7 +12,7 @@ foundFilesCount = 0
 def gatherSphereIlls(filename):
 
     try:
-        f = urllib.request.urlopen("http://dv5bk1m8igv7v.cloudfront.net/asset/21600/content/item/" + filename)
+        f = urllib.request.urlopen("http://dv5bk1m8igv7v.cloudfront.net/asset/21900/content/item/" + filename)
         fetched = f.read()
         f.close()
         
